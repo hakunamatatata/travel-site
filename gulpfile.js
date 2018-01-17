41adeb3913e0d46615758d44da6ff04bf9640486
Thomas <thomas@cantam.ch>
2018-01-17 23:27:59 +0100

    added browsersync and insectcss

--- gulpfile.js
+++ gulpfile.js
@@ -4,26 +4,44 @@ postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
-cssImport = require('postcss-import');
+cssImport = require('postcss-import')
+browserSync = require('browser-sync').create();
 
 gulp.task('default', function() {
   console.log("Hooray - you created a Gulp task!")
 });
 
+// this is not used anymore down here
 gulp.task('html', function() {
   console.log("Gulp watch: ./app/index.html was just changed")
 });
+// until here ^
+
 gulp.task('styles', function() {
   return gulp.src('./app/assets/styles/styles.css')
-  .pipe(postcss([cssImport, nested, cssvars, autoprefixer]))
-  .pipe(gulp.dest('./app/temp/styles'));
+    .pipe(postcss([cssImport, nested, cssvars, autoprefixer]))
+    .pipe(gulp.dest('./app/temp/styles'));
 });
 
 gulp.task('watch', function() {
-  watch ('./app/index.html', function() {
-    gulp.start('html');
+
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "app"
+    }
   });
+
+  watch ('./app/index.html', function() {
+    browserSync.reload();
+  });
+
   watch ('./app/assets/styles/**/*.css', function() {
-    gulp.start('styles');
+    gulp.start('cssInject');
   })
+});
+
+gulp.task('cssInject', ['styles'], function() {
+  gulp.src('./app/temp/styles/styles.css')
+    .pipe(browserSync.stream());
 });
