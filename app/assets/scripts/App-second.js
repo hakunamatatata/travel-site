c8c6dfc2b8382c6d3b074db9faff148a52c9c1ce
hakunamatatata <thomas@cantam.ch>
2018-04-10 21:30:27 +0200

    started using JS finally

---
+++ app/assets/scripts/App-second.js
@@ -0,0 +1,9 @@
+var john = {
+  name: "John Doe",
+  favoriteColor: "blue",
+  greet: function() {
+    console.log("Hello, my name is " + john.name + " and my favorite color is " + john.favoriteColor + ".");
+  }
+}
+
+john.greet();
