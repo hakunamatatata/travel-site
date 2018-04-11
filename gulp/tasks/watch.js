3765cb1b6fd2327de1aaf940ad335af5e80321c6
hakunamatatata <thomas@cantam.ch>
2018-04-11 17:34:05 +0200

    working on gulp stuff

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -18,7 +18,7 @@ gulp.task('watch', function() {
 
   watch ('./app/assets/styles/**/*.css', function() {
     gulp.start('cssInject');
-  })
+  });
 
   watch ('./app/assets/scripts/**/*.js', function() {
     gulp.start('scriptsRefresh');
@@ -32,4 +32,4 @@ gulp.task('cssInject', ['styles'], function() {
 
 gulp.task('scriptsRefresh', ['scripts'], function() {
   browserSync.reload();
-})
+});
